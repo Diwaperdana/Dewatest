610006423acf405453d088c7e85ec50c1f6e96d7
Diwaperdana <35728121+Diwaperdana@users.noreply.github.com>
2018-01-23 18:18:59 +0700

    Add files via upload
    Upload File

---
+++ asset/js/main.js
@@ -0,0 +1,36 @@
+var apiURL = 'https://api.myjson.com/bins/reyab';
+
+new Vue({
+
+  el: '#jobDesigner',
+
+  data() {
+    return {
+      items: []
+    }
+  },
+
+  created: function () {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: function() {
+      var self = this;
+      $.get(apiURL, function(data) {
+        self.items = data.data;
+      });
+
+    }
+
+  }
+});
+
+(function ($){
+  "use strict";
+  $(document).ready(function () {
+    $('#toggle').click(function() {
+    $('.search').toggleClass('reveal');
+    });
+  });
+})(jQuery);
